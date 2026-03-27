f684ea4e2ce481e6d579b21ca9c6cac9a205cb93
Muhammad Ibaadi Ilmi <muhammad.ibaadi@ui.ac.id>
2026-03-27 15:58:32 +0700

    Create home page

---
+++ app/api/background.ts
@@ -0,0 +1,9 @@
+export async function fetchBackgroundColor(): Promise<string> {
+  // TODO: Actually fetch background color from backend
+  return "rgb(0, 127, 255)";
+}
+
+export async function setBackgroundColor(color: string): Promise<void> {
+  // TODO: Send background color change request to backend
+  console.log(color);
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,53 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { useLoaderData } from "react-router-dom";
+import { fetchBackgroundColor, setBackgroundColor } from "../api/background";
+import { useEffect } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "SQL Inject IP4" },
+    { name: "description", content: "Halo dunia'); DROP TABLE Groups;--" },
   ];
 }
 
+export async function loader() {
+  const color = await fetchBackgroundColor();
+  return { color };
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const formData = new FormData(e.currentTarget);
+
+  // TODO: Input validation/sanitization
+  const [r, g, b] = ["r", "g", "b"].map(c => parseInt(formData.get(c) as string));
+
+  const color = `rgb(${r}, ${g}, ${b})`;
+
+  setBackgroundColor(color);
+  
+}
+
 export default function Home() {
-  return <Welcome />;
+  const { color } = useLoaderData() as { color: string };
+
+  useEffect(() => {
+    document.body.style.backgroundColor = color;
+  }, [color]);
+
+  return <div>
+    <h1>SQL Inject IP4</h1>
+    <div id="change-bg-container">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="r">R</label>
+        <input type="number" id="r" name="r" min="0" max="255"/>
+        <label htmlFor="g">G</label>
+        <input type="number" id="g" name="g" min="0" max="255"/>
+        <label htmlFor="b">B</label>
+        <input type="number" id="b" name="b" min="0" max="255"/>
+        <input type="submit" value="Change Background Color"/>
+      </form>
+    </div>
+  </div>;
 }
