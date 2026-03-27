366504f7f51e4488705403c882df737df22a5d60
Muhammad Ibaadi Ilmi <muhammad.ibaadi@ui.ac.id>
2026-03-27 16:34:51 +0700

    Styling

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -51,20 +51,24 @@ export default function Home() {
   return <div>
     <h1>SQL Inject IP4</h1>
     <div id="change-bg-container">
+      <h2>Change Background Color</h2>
       <form onSubmit={handleSubmitBackground}>
-        <label htmlFor="r">R</label>
+        <label htmlFor="r">R: </label>
         <input type="number" id="r" name="r" min="0" max="255"/>
-        <label htmlFor="g">G</label>
+        <label htmlFor="g">G: </label>
         <input type="number" id="g" name="g" min="0" max="255"/>
-        <label htmlFor="b">B</label>
+        <label htmlFor="b">B: </label>
         <input type="number" id="b" name="b" min="0" max="255"/>
+        <br />
         <input type="submit" value="Change Background Color"/>
       </form>
     </div>
     <div id="change-font-container">
+      <h2>Change Font</h2>
       <form onSubmit={handleSubmitFont}>
         <label htmlFor="r">New font name: </label>
         <input type="text" id="new-font" name="new-font" required/>
+        <br />
         <input type="submit" value="Change Font"/>
       </form>
     </div>
