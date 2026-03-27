ec29b5748d3a928c3e34a6d8e8f5bd3fd2f2a4e3
Muhammad Ibaadi Ilmi <muhammad.ibaadi@ui.ac.id>
2026-03-27 16:20:29 +0700

    Add change font form

--- app/api/background.ts
+++ app/api/background.ts
@@ -7,3 +7,13 @@ export async function sendBackgroundColor(color: string): Promise<void> {
   // TODO: Send background color change request to backend
   console.log(color);
 }
+
+export async function fetchFont(): Promise<string> {
+  // TODO: Actually fetch background color from backend
+  return "Comic Sans MS";
+}
+
+export async function sendFont(font: string): Promise<void> {
+  // TODO: Send background color change request to backend
+  console.log(font);
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,6 @@
 import type { Route } from "./+types/home";
 import { useLoaderData } from "react-router-dom";
-import { fetchBackgroundColor, sendBackgroundColor } from "../api/background";
+import { fetchBackgroundColor, sendBackgroundColor, fetchFont, sendFont } from "../api/background";
 import { useEffect } from "react";
 
 export function meta({}: Route.MetaArgs) {
@@ -12,10 +12,11 @@ export function meta({}: Route.MetaArgs) {
 
 export async function loader() {
   const color = await fetchBackgroundColor();
-  return { color };
+  const font = await fetchFont();
+  return { color, font };
 }
 
-function handleSubmit(e) {
+function handleSubmitBackground(e) {
   e.preventDefault();
 
   const formData = new FormData(e.currentTarget);
@@ -28,17 +29,29 @@ function handleSubmit(e) {
   sendBackgroundColor(color);
 }
 
+function handleSubmitFont(e) {
+  e.preventDefault();
+
+  const formData = new FormData(e.currentTarget);
+
+  // TODO: Input validation/sanitization
+  const font = formData.get("new-font") as string;
+
+  sendFont(font);
+}
+
 export default function Home() {
-  const { color } = useLoaderData() as { color: string };
+  const { color, font } = useLoaderData() as { color: string, font: string };
 
   useEffect(() => {
     document.body.style.backgroundColor = color;
-  }, [color]);
+    document.body.style.fontFamily = font;
+  }, [color, font]);
 
   return <div>
     <h1>SQL Inject IP4</h1>
     <div id="change-bg-container">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmitBackground}>
         <label htmlFor="r">R</label>
         <input type="number" id="r" name="r" min="0" max="255"/>
         <label htmlFor="g">G</label>
@@ -46,6 +59,13 @@ export default function Home() {
         <label htmlFor="b">B</label>
         <input type="number" id="b" name="b" min="0" max="255"/>
         <input type="submit" value="Change Background Color"/>
+      </form>
+    </div>
+    <div id="change-font-container">
+      <form onSubmit={handleSubmitFont}>
+        <label htmlFor="r">New font name: </label>
+        <input type="text" id="new-font" name="new-font" required/>
+        <input type="submit" value="Change Font"/>
       </form>
     </div>
   </div>;
